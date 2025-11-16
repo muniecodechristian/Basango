3a2be3b25ee46946b7be27df50ebdb5077a11b0a
muniecodechristian <muniecode@gmail.com>
2025-11-16 16:15:00 +0100

    Mise à jour du projet

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,15 @@
 import {Routes, Route } from 'react-router-dom'; 
 import Layout from "../pages/layout";
 import Home from "../pages/Home/page";
-import Saved from "../pages/Saved/page";
-import Category from "../pages/category/page";
-import Suggestion from "../pages/suggestion/page";
-import Error from '../pages/error/page'
+
 
 export default function AppRouter() {
   return (
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/suggested" element={<Suggestion />} />
-          <Route path="/saved" element={<Saved />} />
-          <Route path="*" element={<Error />} />
+        
+        
         </Route>
       </Routes>
   )
